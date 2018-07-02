6a7251a6c5924d8bbc5c2ecff07dafa6ff6b9425
ejbohnert <ejbohnert@outlook.com>
2018-07-02 13:54:22 -0500

    Added BreadController as well as IngredientsController.  Also added Views and Models for each.

---
+++ BreadBuilder/Controllers/BreadController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BreadBuilder.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BreadBuilder.Controllers
+{
+    public class BreadController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<Bread> breads = new List<Bread>();
+
+            return View(breads);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
---
+++ BreadBuilder/Models/Bread.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BreadBuilder.Models
+{
+    public class Bread
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public IList<Ingredient> ingredients { get; set; }
+
+    }
+}

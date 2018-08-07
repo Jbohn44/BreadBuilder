6065cb25f38950a1497d5e12a889c32a8cb3c990
ejbohnert <ejbohnert@outlook.com>
2018-08-06 20:32:48 -0500

    Added User Recipe List page

--- BreadBuilder/Controllers/UserController.cs
+++ BreadBuilder/Controllers/UserController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BreadBuilder.Data;
+using BreadBuilder.Models;
 using BreadBuilder.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,6 +11,13 @@ namespace BreadBuilder.Controllers
 {
     public class UserController : Controller
     {
+        private BreadDbContext context;
+
+        public UserController(BreadDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
         public IActionResult Index()
         {
             return Redirect("Add");
@@ -19,6 +28,12 @@ namespace BreadBuilder.Controllers
             AddUserViewModel addUserViewModel = new AddUserViewModel();
 
             return View(addUserViewModel);
+        }
+
+        public IActionResult UserRecipeList()
+        {
+            List<Bread> breads = context.Breads.ToList();
+            return View(breads);
         }
     }
 }

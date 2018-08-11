67d3dfe6bdf86a4a2cd2a37cdd598ce79c4e448c
ejbohnert <ejbohnert@outlook.com>
2018-08-11 16:38:43 -0500

    add ingredient button on add form

--- BreadBuilder/wwwroot/js/site.js
+++ BreadBuilder/wwwroot/js/site.js
@@ -1,1 +1,27 @@
 ﻿// Write your JavaScript code.
+
+
+var formPopulate =
+    '<span class="form-inline">' +
+    '<label asp-for="Ingredient.Name">Ingredient</label>' +
+    '<input asp-for="Ingredient.Name" class="form-control" />' +
+
+    '<label asp-for="Measurement.Value">Measurement</label>' +
+    '<input asp-for="Measurement.Value" class="form-control" />' +
+
+    '<label asp-for="Measurement.Unit">Unit</label>' +
+    '<select asp-for="Measurement.Unit" asp-items="Model.MeasurementUnits"></select>' +
+    '<button onclick="addInput()">Click to add Stuff</button>' +
+    '</span>';
+
+
+
+
+function addInput()
+{
+    document.getElementById("wrapper").innerHTML = formPopulate;
+
+}
+
+
+             

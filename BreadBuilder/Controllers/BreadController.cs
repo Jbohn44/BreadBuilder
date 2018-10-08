5caaa663371a9d79803f30a3c6f6067d5f9fa3a2
ejbohnert <ejbohnert@outlook.com>
2018-10-08 12:39:25 -0500

    changed the css on login and userrecipelist pages

--- BreadBuilder/Controllers/BreadController.cs
+++ BreadBuilder/Controllers/BreadController.cs
@@ -37,7 +37,7 @@ namespace BreadBuilder.Controllers
         {
             if (ModelState.IsValid)
             {
-                int userId = Convert.ToInt32(TempData["UserId"].ToString());
+                int userId = Convert.ToInt32(TempData["UserId"].ToString()); //converts the user id to an integer from tempdata
                 //holds the list of recipe items from viewmodel
                 List<RecipeItem> RecipeItemList = new List<RecipeItem>();
 

4299a540cb722b300fdc2b7dcf61ff7781fa6e51
ejbohnert <ejbohnert@outlook.com>
2018-11-29 12:17:47 -0600

    edited HashPass.cs

--- BreadBuilder/Models/HashPass.cs
+++ BreadBuilder/Models/HashPass.cs
@@ -8,23 +8,13 @@ namespace BreadBuilder.Models
 {
     public static class HashPass
     {
-        
-            /// <summary>
-            /// Size of salt.
-            /// </summary>
+       
             private const int SaltSize = 16;
 
-            /// <summary>
-            /// Size of hash.
-            /// </summary>
+           
             private const int HashSize = 20;
 
-            /// <summary>
-            /// Creates a hash from a password.
-            /// </summary>
-            /// <param name="password">The password.</param>
-            /// <param name="iterations">Number of iterations.</param>
-            /// <returns>The hash.</returns>
+          
             public static string Hash(string password, int iterations)
             {
                 // Create salt
@@ -39,40 +29,25 @@ namespace BreadBuilder.Models
                 var hashBytes = new byte[SaltSize + HashSize];
                 Array.Copy(salt, 0, hashBytes, 0, SaltSize);
                 Array.Copy(hash, 0, hashBytes, SaltSize, HashSize);
-
-                // Convert to base64
+            
                 var base64Hash = Convert.ToBase64String(hashBytes);
 
                 // Format hash with extra information
                 return string.Format("$MYHASH$V1${0}${1}", iterations, base64Hash);
             }
 
-            /// <summary>
-            /// Creates a hash from a password with 10000 iterations
-            /// </summary>
-            /// <param name="password">The password.</param>
-            /// <returns>The hash.</returns>
+          
             public static string Hash(string password)
             {
                 return Hash(password, 10000);
             }
 
-            /// <summary>
-            /// Checks if hash is supported.
-            /// </summary>
-            /// <param name="hashString">The hash.</param>
-            /// <returns>Is supported?</returns>
             public static bool IsHashSupported(string hashString)
             {
                 return hashString.Contains("$MYHASH$V1$");
             }
 
-            /// <summary>
-            /// Verifies a password against a hash.
-            /// </summary>
-            /// <param name="password">The password.</param>
-            /// <param name="hashedPassword">The hash.</param>
-            /// <returns>Could be verified?</returns>
+       
             public static bool Verify(string password, string hashedPassword)
             {
                 // Check hash
